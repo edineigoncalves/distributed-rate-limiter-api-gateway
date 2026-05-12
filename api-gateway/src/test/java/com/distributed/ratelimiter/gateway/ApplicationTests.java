1b8a37951236b6d54ab157c0f0289a5c6b50ba74
edineigoncalves <edineigoncalvesfigueiredo@gmail.com>
2026-05-12 02:12:06 -0300

    feature: add folders and routes

---
+++ api-gateway/src/test/java/com/distributed/ratelimiter/gateway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed.ratelimiter.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
