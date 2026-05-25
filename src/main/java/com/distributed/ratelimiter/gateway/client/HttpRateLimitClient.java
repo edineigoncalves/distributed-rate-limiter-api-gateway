0cc92d1e0af41e416f9093347450aea3e09a4d9b
edineigoncalves <edineigoncalvesfigueiredo@gmail.com>
2026-05-24 23:52:22 -0300

    feat(gateway): add HttpRateLimitClient stub

--- src/main/java/com/distributed/ratelimiter/gateway/client/HttpRateLimitClient.java
+++ src/main/java/com/distributed/ratelimiter/gateway/client/HttpRateLimitClient.java
@@ -3,14 +3,15 @@ package com.distributed.ratelimiter.gateway.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 @Component
-public class HttpRateLimitClient implements RateLimitClient{
+public class HttpRateLimitClient implements RateLimitClient {
 
     private static final Logger log = LoggerFactory.getLogger(HttpRateLimitClient.class);
 
     @Override
     public CheckResult check(CheckRequest request) {
         log.info("HttpRateLimitClient stub called for tenant={}", request.tenant());
-        return CheckResult.allow(99,100, System.currentTimeMillis()/100+60);
+        return CheckResult.allow(99, 100, System.currentTimeMillis() / 1000 + 60);
     }
 }
